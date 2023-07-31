81649852a94659da338929289410294bf11c27ab
Dmitriy Aleksandrovich Chernukho <gormonn@redcollar.ru>
2023-07-31 06:03:48 +0300

    rm dead code

--- src/sw/index.ts
+++
@@ -1,6 +0,0 @@
-import * as Comlink from 'comlink';
-import type { WorkerMethods } from './worker';
-import AppWorker from './worker?worker';
-
-const workerMethods = new AppWorker();
-export const workerInstance = Comlink.wrap<typeof WorkerMethods>(workerMethods);
