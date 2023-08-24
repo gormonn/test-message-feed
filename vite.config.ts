7e9d5b6a4da00df015af4ad03aff8da809a7ac68
Dmitriy Aleksandrovich Chernukho <gormonn@redcollar.ru>
2023-08-24 03:41:21 +0300

    update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-const base = process.env.PREFIX_PATHS ? '/<REPO>/' : undefined;
+const base = process.env.PREFIX_PATHS ? '/test-message-feed/' : undefined;
 
 // https://vitejs.dev/config/
 export default defineConfig({
